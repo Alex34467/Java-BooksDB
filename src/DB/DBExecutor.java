92acb8da991e77ab553caca455e684f6685b43dc
Alex34467 <borisov.alexey0@gmail.com>
2018-01-29 20:01:04 +0300

    Добавлен метод connect.

--- src/DB/DBExecutor.java
+++ src/DB/DBExecutor.java
@@ -6,6 +6,9 @@ import java.sql.ResultSet;
 // Интерфейс исполнителя кодманд к базе данных.
 public interface DBExecutor
 {
+    // Подключение к БД.
+    void connect(String url);
+
     // Запрос без езультата.
     void executeUpdate(String query);
 
