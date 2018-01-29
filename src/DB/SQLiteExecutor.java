822878229b05ac91adc493fb318876656e6a527e
Alex34467 <borisov.alexey0@gmail.com>
2018-01-29 20:03:11 +0300

    Добавлен метод connect. Реализован метод executeUpdate.

--- src/DB/SQLiteExecutor.java
+++ src/DB/SQLiteExecutor.java
@@ -1,17 +1,46 @@
 package DB;
 
+import org.sqlite.JDBC;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-import java.sql.ResultSet;
+import java.sql.*;
 
 
 // Исполнитель команд к базе данных.
 public class SQLiteExecutor implements DBExecutor
 {
+    // Данные.
+    private Connection connection;
+    private Statement statement;
+
+
+    // Подключение к базе данных.
+    @Override
+    public void connect(String url)
+    {
+        try
+        {
+            DriverManager.registerDriver(new JDBC());
+            connection = DriverManager.getConnection(url);
+            statement = connection.createStatement();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     // Запрос без результата.
     @Override
     public void executeUpdate(String query)
     {
-        throw new NotImplementedException();
+        try
+        {
+            statement.executeUpdate(query);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     // Запрос с результатаом.
