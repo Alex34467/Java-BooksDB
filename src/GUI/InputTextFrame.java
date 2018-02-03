4486c63256000f8b0dca002a1600650fb108a7ba
Alex34467 <borisov.alexey0@gmail.com>
2018-02-03 16:44:32 +0300

    Класс AuthorsFrame переименован в InputTextFrame.

--- src/GUI/InputTextFrame.java
+++ src/GUI/InputTextFrame.java
@@ -9,27 +9,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-// Окно добавления авторов.
-public class AuthorsFrame extends JDialog
+// Окно для ввода текста.
+public class InputTextFrame extends JDialog
 {
     // Данные.
-    private String[] authors;
-    private DefaultListModel<String> authorsListModel = new DefaultListModel<>();
+    private String[] data;
+    private DefaultListModel<String> textListModel = new DefaultListModel<>();
     private ButtonClickListener buttonClickListener;
     private MyDocumentListener documentListener;
 
     // Элементы.
-    private JTextField authorTextField;
-    private JList<String> authorsList;
+    private JTextField textField;
+    private JList<String> textList;
 
 
     // Конструктор.
-    public AuthorsFrame(JDialog owner, String title, boolean modal)
+    public InputTextFrame(JDialog owner, String title, String[] values, boolean modal)
     {
+        // Инициализация окна.
         super(owner, title, modal);
         setBounds(500, 500, 150, 250);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        // Инициализация списка значений.
+        data = values;
 
         // Инициализация обработчиков.
         buttonClickListener = new ButtonClickListener();
@@ -42,9 +46,11 @@ public class AuthorsFrame extends JDialog
     // Отображение.
     public String showDialog()
     {
+        // Показ окна.
         setVisible(true);
 
-        return authorTextField.getText();
+        // Воврат введенного текста.
+        return textField.getText();
     }
 
     // Создание интерфейса.
@@ -53,37 +59,37 @@ public class AuthorsFrame extends JDialog
         // Инициализация контейнера.
         Container container = getContentPane();
 
-        // Инициализация поля ввода авторов.
-        authorTextField = new JTextField(20);
-        authorTextField.getDocument().addDocumentListener(documentListener);
+        // Инициализация поля ввода.
+        textField = new JTextField(20);
+        textField.getDocument().addDocumentListener(documentListener);
 
-        // Инициализация списка авторов.
-        authorsList = new JList<>(authorsListModel);
-        JScrollPane authorsScrollPane = new JScrollPane();
-        authorsScrollPane.setViewportView(authorsList);
+        // Инициализация списка.
+        textList = new JList<>(textListModel);
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportView(textList);
 
-        // Инициализация панели кнопок авторов.
-        JPanel authorsButtonsPanel = new JPanel();
-        authorsButtonsPanel.setPreferredSize(new Dimension(150, 26));
-        authorsButtonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        // Инициализация панели кнопок.
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setPreferredSize(new Dimension(150, 26));
+        buttonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 
-        // Инициализация кнопок действий авторов.
-        JButton addAuthorButton = new JButton("OK");
-        addAuthorButton.setActionCommand("Add");
-        addAuthorButton.addActionListener(buttonClickListener);
-        JButton deleteAuthorButton = new JButton("Отмена");
-        deleteAuthorButton.setActionCommand("Cancel");
-        deleteAuthorButton.addActionListener(buttonClickListener);
+        // Инициализация кнопок действий.
+        JButton okButton = new JButton("OK");
+        okButton.setActionCommand("Add");
+        okButton.addActionListener(buttonClickListener);
+        JButton cancelButton = new JButton("Отмена");
+        cancelButton.setActionCommand("Cancel");
+        cancelButton.addActionListener(buttonClickListener);
 
-        // Добавдение кнопок действий авторов на панель кнопок авторов.
-        authorsButtonsPanel.add(addAuthorButton);
-        authorsButtonsPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-        authorsButtonsPanel.add(deleteAuthorButton);
+        // Добавдение кнопок действий на панель кнопок.
+        buttonsPanel.add(okButton);
+        buttonsPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+        buttonsPanel.add(cancelButton);
 
         // Добавление элементов.
-        container.add(authorTextField, BorderLayout.PAGE_START);
-        container.add(authorsScrollPane, BorderLayout.CENTER);
-        container.add(authorsButtonsPanel, BorderLayout.PAGE_END);
+        container.add(textField, BorderLayout.PAGE_START);
+        container.add(scrollPane, BorderLayout.CENTER);
+        container.add(buttonsPanel, BorderLayout.PAGE_END);
     }
 
     // Обработчик событий нажантия кнопок.
@@ -101,7 +107,7 @@ public class AuthorsFrame extends JDialog
             {
                 case "Add":
                 case "Cancel":
-                    AuthorsFrame.this.dispose();
+                    InputTextFrame.this.dispose();
                     break;
             }
         }
@@ -114,14 +120,14 @@ public class AuthorsFrame extends JDialog
         @Override
         public void insertUpdate(DocumentEvent e)
         {
-            ListModelTools.filterByText(authorsListModel, authorTextField.getText(), authors, true);
+            ListModelTools.filterByText(textListModel, textField.getText(), data, true);
         }
 
         // Удаление текста.
         @Override
         public void removeUpdate(DocumentEvent e)
         {
-            ListModelTools.filterByText(authorsListModel, authorTextField.getText(), authors, true);
+            ListModelTools.filterByText(textListModel, textField.getText(), data, true);
         }
 
         // Изменение текста.
