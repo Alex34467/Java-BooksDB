454f1f954dd8242185455ed997b34d31248954d2
Alex34467 <borisov.alexey0@gmail.com>
2018-03-30 16:11:39 +0300

    Добавлен класс ValidationTools, проверяющий данные.

---
+++ src/Tools/ValidationTools.java
@@ -0,0 +1,30 @@
+package Tools;
+
+import java.util.Calendar;
+
+// Класс для валидации.
+public class ValidationTools
+{
+    // Проверка года.
+    public static boolean validateYear(String yearStr)
+    {
+        // Проверка наличия стороки.
+        if (yearStr.isEmpty()) return false;
+
+        try
+        {
+            // Проверка на число.
+            int year = Integer.parseInt(yearStr);
+
+            // Проверка значения года.
+            if (year <= 1500 || year > Calendar.getInstance().get(Calendar.YEAR)) return false;
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+
+        // Возврат результата.
+        return true;
+    }
+}
