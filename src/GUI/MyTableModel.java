1ec4856eeb35696a7a2a8d9f982381a7a1d51194
Alex34467 <borisov.alexey0@gmail.com>
2018-04-18 16:05:47 +0300

    Рефакторинг метода getRowsCount. Рефакторинг метода getValue. Добавлен метод isCellEditable.

--- src/GUI/MyTableModel.java
+++ src/GUI/MyTableModel.java
@@ -1,16 +1,23 @@
 package GUI;
 
+import DB.DBService;
+import Entities.BooksViewEntity;
 import javax.swing.table.AbstractTableModel;
+import java.util.List;
 
 
 // Модель таблицы.
 public class MyTableModel extends AbstractTableModel
 {
+    // Данные.
+    private List<BooksViewEntity> booksViewEntities = DBService.getInstance().getAllBooksViewEntities();
+
+
     // Возврат количества строк.
     @Override
     public int getRowCount()
     {
-        return 50;
+        return booksViewEntities.size();
     }
 
     // Возврат количества столбцов.
@@ -24,7 +31,32 @@ public class MyTableModel extends AbstractTableModel
     @Override
     public Object getValueAt(int rowIndex, int columnIndex)
     {
-        return null;
+        // Получение объекта.
+        BooksViewEntity booksViewEntity = booksViewEntities.get(rowIndex);
+
+        // Возврат значений.
+        switch (columnIndex)
+        {
+            case 0:
+                return booksViewEntity.getAuthors();
+            case 1:
+                return booksViewEntity.getName();
+            case 2:
+                return booksViewEntity.getYear();
+            case 3:
+                return booksViewEntity.getLanguage();
+            case 4:
+                return booksViewEntity.getReadState();
+            default:
+                return null;
+        }
+    }
+
+    // Возврат флага редактирования ячейки.
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex)
+    {
+        return false;
     }
 
     // Возврат имени слолбца.
